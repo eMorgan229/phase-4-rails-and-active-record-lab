5bb1f7728fca9d5cd72df57727f66f491e607da9
Em <ekramm@me.com>
2022-06-27 16:17:40 -0400

    commit tree 8e96db960cb36e2fa91ce16a7f6f178630cddb23

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create(first_name: "Sam", last_name: "Black", grade: 6, created_at: 1124134, updated_at: 5341235)
+Student.create(first_name: "Bob", last_name: "Jones", grade: 6, created_at: 1124134, updated_at: 5341235)
